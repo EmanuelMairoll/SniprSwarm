5bbc762effc847a9551de013325c10e0da9b7da9
Emanuel Mairoll <accounts@emanuelmairoll.at>
2021-04-25 11:46:41 +0200

    Project Setup

---
+++ SniprSwarm/SniprSwarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SniprSwarmApp.swift
+//  SniprSwarm
+//
+//  Created by Emanuel Mairoll on 25.04.21.
+//
+
+import SwiftUI
+
+@main
+struct SniprSwarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
